62b919dd8ea5863be056eda5fca123f96d49ecda
jsurt <jacksurtees@gmail.com>
2019-05-08 16:43:52 -0400

    added recommended revisions

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ $(() => {
   $(".icon.linkedin").mouseover(() => {
     $(".contact-label").empty();
     $(".linkedin-label").html(
-      '<a href="https://www.linkedin.com/in/john-surtees">www.linkedin.com/in/john-surtees</a>'
+      '<a href="https://www.linkedin.com/in/john-surtees" target="_blank">www.linkedin.com/in/john-surtees</a>'
     );
   });
   $(".icon.github").mouseover(() => {
     $(".contact-label").empty();
     $(".github-label").html(
-      '<a href="https://github.com/jsurt">https://github.com/jsurt</a>'
+      '<a href="https://github.com/jsurt" target="_blank">https://github.com/jsurt</a>'
     );
   });
   $(".icon.gmail").on("touchstart", () => {
@@ -23,13 +23,13 @@ $(() => {
   $(".icon.linkedin").on("touchstart", () => {
     $(".contact-label").empty();
     $(".linkedin-label").html(
-      '<a href="https://www.linkedin.com/in/john-surtees">www.linkedin.com/in/john-surtees</a>'
+      '<a href="https://www.linkedin.com/in/john-surtees" target="_blank">www.linkedin.com/in/john-surtees</a>'
     );
   });
   $(".icon.github").on("touchstart", () => {
     $(".contact-label").empty();
     $(".github-label").html(
-      '<a href="https://github.com/jsurt">https://github.com/jsurt</a>'
+      '<a href="https://github.com/jsurt" target="_blank">https://github.com/jsurt</a>'
     );
   });
   $("#contact").click(() => {
